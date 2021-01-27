c8776c0e843bce92e8ae2365b0ea27a383c71978
zplotimkun <timkun_@hotmail.com>
2021-01-27 19:28:47 +0800

    build test

--- order_web/order_app/views.py
+++ order_web/order_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.sessions.models import Session
 
 # Create your views here.
+
+
+def index_page(request):
+    return render(request, 'index.html', {})
--- order_web/order_web/urls.py
+++ order_web/order_web/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from order_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index_page, name='index'),
 ]
